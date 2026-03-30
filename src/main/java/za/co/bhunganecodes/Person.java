5f507cfb29ee32a15a372b56dd669017f77bfe1b
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-03-30 21:44:25 +0200

    Added a new Person parent class

---
+++ src/main/java/za/co/bhunganecodes/Person.java
@@ -0,0 +1,37 @@
+package za.co.bhunganecodes;
+
+class Person {
+    private String name;
+    private int age;
+    private double height;
+
+    Person(String name, int age, double height){
+        this.setName(name);
+        this.setAge(age);
+        this.setHeight(height);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int newAge){
+        this.age = newAge;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public void setHeight(double newHeight){
+        this.height = newHeight;
+    }
+}
